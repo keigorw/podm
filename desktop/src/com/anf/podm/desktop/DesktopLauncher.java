0a85274e778e2867deb7055d941df6b41b59c104
Seth <Seth@192.168.1.107>
2015-12-22 12:22:13 -0600

    Initial commit

---
+++ desktop/src/com/anf/podm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.anf.podm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.anf.podm.podmGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new podmGame(), config);
+	}
+}
